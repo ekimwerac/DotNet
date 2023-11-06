e2b2eddb9f30da3842608a2fa96e510885f3d78a
Mike Carew <mike@carew.uk>
2023-11-06 23:16:34 +0000

    first commit

---
+++ extensions/MyExtensions.cs
@@ -0,0 +1,13 @@
+namespace MyExtensions
+{
+    public static class StringExtensions
+    {
+        public static string CapitalizeFirstLetter(this string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return input;
+
+            return char.ToUpper(input[0]) + input.Substring(1);
+        }
+    }
+}
---
+++ extensions/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+using MyExtensions; // Import the namespace where the extension methods are defined
+
+class Program
+{
+    static void Main()
+    {
+        string text = "hello, world!";
+        string capitalized = text.CapitalizeFirstLetter();
+        Console.WriteLine(capitalized); // Output: "Hello, world!"
+    }
+}
